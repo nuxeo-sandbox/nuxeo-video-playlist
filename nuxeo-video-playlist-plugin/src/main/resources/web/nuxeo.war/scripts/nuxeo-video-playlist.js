7075f536056dd38037bb860f0b192968953ccbdb
Thibaud Arguillere <targuillere@nuxeo.com>
2015-09-29 10:25:29 -0400

    Not that far

--- nuxeo-video-playlist-plugin/src/main/resources/web/nuxeo.war/scripts/nuxeo-video-playlist.js
+++ nuxeo-video-playlist-plugin/src/main/resources/web/nuxeo.war/scripts/nuxeo-video-playlist.js
@@ -36,7 +36,7 @@
 var ID_SUFFIX;
 var MAIN_DIV_ID, VIDEO_ID, PLAYLIST_ID, FIRST_BUTTON_ID, PREV_BUTTON_ID, NEXT_BUTTON_ID, LAST_BUTTON_ID;
 
-var gVideos = [], gPlayer, gEls = {};
+var gVideos = [], gPlayer, gEls = {}, gDragSrcElt, gVideosAfterDD;
 
 if (typeof NcVPL_INIT_COUNTER === "undefined" || NcVPL_INIT_COUNTER === null) {
     NcVPL_INIT_COUNTER = 0;
@@ -69,12 +69,12 @@ function NuxeoVideoPlaylist_Init(inIdSuffix, inPlaylistJson) {
             return;
         }
 
-        console.log("INIT NxVPL");
+        //console.log("INIT NxVPL");
         NxVPL = {
 
             init : function() {
                 gPlayer = vjs(document.getElementById(VIDEO_ID));
-
+                
                 NxVPL.cacheElements();
                 NxVPL.initVideos();
                 NxVPL.createListOfVideos();
@@ -92,18 +92,122 @@ function NuxeoVideoPlaylist_Init(inIdSuffix, inPlaylistJson) {
             },
 
             initVideos : function() {
-                gPlayer.playList(gVideos);
+                // The array received by videojs-playlist is kept by reference (playlist doesn not do a deep copy).
+                // And as it modifies the array, to add some custom infos, we must pass a deep-copy, because later,
+                // if the user drag-drop, we reuse the original gVideos
+                var deepCopy = JSON.parse(JSON.stringify(gVideos));; //jQuery.extend(true, {}, gVideos);
+                gPlayer.playList(deepCopy);
+                //gPlayer.playList(gVideos);
             },
 
             createListOfVideos : function() {
                 var html = '';
                 for (var i = 0, len = gVideos.length; i < len; i++) {
+                    /*
                     html += '<li id="vpl-video-' + i + '" data-videoplaylist="' + i + '">' + '<span class="number">' + (i + 1) + '</span>'
                             + '<span class="poster"><img src="' + gVideos[i].poster + '"></span>'
-                            + '<span class="title">' + gVideos[i].title + '</span>' + '</li>';
+                            + '<span class="title">' + gVideos[i].title + '</span>' + '</li>';*/
+                    html += '<li data-videoplaylist="' + i + '">' + '<div id="vpl-video-' + i + '" draggable=true>'
+                            + '<span class="number">' + (i + 1) + '</span>' + '<span class="poster"><img src="'
+                            + gVideos[i].poster + '"></span>' + '<span class="title">' + gVideos[i].title + '</span>'
+                            + '</div></li>';
                 }
                 gEls.$playlist.empty().html(html);
                 NxVPL.updateActiveVideo();
+
+                // Workaround for FF, which needs a minimum of data bound to a drag handler
+                // Anyway, we must setup the drag handlers for all items
+                setTimeout(function() {
+
+                    var theItem, i, len;
+                    len = gVideos.length;
+                    for (i = 0; i < len; i++) {
+                        theItem = document.getElementById("vpl-video-" + i);
+
+                        theItem.addEventListener('dragstart', function(e) {
+
+                            gDragSrcElt = this;
+                            gDragSrcElt.vpl_originaIndex = parseInt(gDragSrcElt.id.replace("vpl-video-", ""));
+
+                            // FireFox _wants_ something in the drag-data.
+                            e.dataTransfer.setData('text', this.id);
+                        });
+
+                        theItem.addEventListener('dragenter', function(e) {
+
+                            if (gDragSrcElt != this) {
+                                this.classList.add("vpl_dragOver");
+                            }
+                        }, false);
+
+                        theItem.addEventListener('dragover', function(e) {
+
+                            if (e.preventDefault) {
+                                e.preventDefault(); // Necessary. Allows us to drop.
+                            }
+
+                            return false;
+
+                        }, false);
+
+                        theItem.addEventListener('dragleave', function(e) {
+                            this.classList.remove("vpl_dragOver");
+                        }, false);
+
+                        theItem.addEventListener('drop',
+                                function(e) {
+
+                                    var destIdx, srcIdx, count;
+                                    var theIdx, theLen; // do not use i nor len, they are in the outer loop already;
+
+                                    if (e.stopPropagation) {
+                                        e.stopPropagation(); // stops the browser from redirecting.
+                                    }
+
+                                    if (gDragSrcElt != this) {
+                                        srcIdx = gDragSrcElt.vpl_originaIndex;
+                                        destIdx = parseInt(this.id.replace("vpl-video-", ""));
+
+                                        gVideosAfterDD = [];
+                                        theLen = gVideos.length;
+                                        for (theIdx = 0; theIdx < theLen; ++theIdx) {
+                                            gVideosAfterDD.push(gVideos[theIdx]);
+                                        }
+                                        
+                                        vplp_utils_move(gVideosAfterDD, srcIdx, destIdx);
+
+                                    }
+                                    gDragSrcElt = null;
+
+                                    setTimeout(function() {
+                                        
+                                        gVideos = [];
+                                        theLen = gVideosAfterDD.length;
+                                        for (theIdx = 0; theIdx < theLen; ++theIdx) {
+                                            gVideos.push(gVideosAfterDD[theIdx]);
+                                        }
+                                        
+                                        NxVPL.initVideos();
+                                        NxVPL.createListOfVideos();
+                                        NxVPL.bindEvents();
+                                        NxVPL.goToFirst();
+                                        
+                                    }, 200);
+
+                                    return false;
+
+                                }, false);
+
+                        theItem.addEventListener('dragend', function(e) {
+                            for (var i = 0, len = gVideos.length; i < len; i++) {
+                                theItem = document.getElementById("vpl-video-" + i);
+                                theItem.classList.remove("vpl_dragOver");
+                            }
+
+                            return false;
+                        }, false);
+                    }
+                }, 500);
             },
 
             updateActiveVideo : function() {
@@ -171,35 +275,51 @@ function NuxeoVideoPlaylist_Init(inIdSuffix, inPlaylistJson) {
                 // Still this "2 init calls" problem (see header of this script)
                 // But here, we'd like to be in the second call...
                 // So, let's do the usual workaround, with a timeout :-<
-                setTimeout(function() {
-                    var playlistObj = jQuery(document.getElementById(PLAYLIST_ID));
-                    if (playlistObj.width() > 700) {
-                        setTimeout(alignElements, 50);
-                    } else {
-                        var fancyboxContainerObj, mainDivObj, videoObj, playlistObj, mainWidth, mainHeight, marginH, marginV;
+                setTimeout(
+                        function() {
+                            var playlistObj = jQuery(document.getElementById(PLAYLIST_ID));
+                            if (playlistObj.width() > 700) {
+                                setTimeout(alignElements, 50);
+                            } else {
+                                var fancyboxContainerObj, mainDivObj, videoObj, playlistObj, mainWidth, mainHeight, marginH, marginV;
 
-                        fancyboxContainerObj = jQuery("#fancybox-content");
-                        mainDivObj = jQuery(document.getElementById(MAIN_DIV_ID));
-                        videoObj = jQuery(document.getElementById(VIDEO_ID));
-                        playlistObj = jQuery(document.getElementById(PLAYLIST_ID));
+                                fancyboxContainerObj = jQuery("#fancybox-content");
+                                mainDivObj = jQuery(document.getElementById(MAIN_DIV_ID));
+                                videoObj = jQuery(document.getElementById(VIDEO_ID));
+                                playlistObj = jQuery(document.getElementById(PLAYLIST_ID));
 
-                        mainWidth = fancyboxContainerObj.width();
-                        mainDivObj.width(videoObj.width() + playlistObj.width() + 4);
-                        marginH = (fancyboxContainerObj.width() - mainDivObj.width()) / 2;
+                                mainWidth = fancyboxContainerObj.width();
+                                mainDivObj.width(videoObj.width() + playlistObj.width() + 4);
+                                marginH = (fancyboxContainerObj.width() - mainDivObj.width()) / 2;
 
-                        marginV = ((fancyboxContainerObj.height() - mainDivObj.position().top) - mainDivObj.height()) / 2;
+                                marginV = ((fancyboxContainerObj.height() - mainDivObj.position().top) - mainDivObj
+                                        .height()) / 2;
 
-                        mainDivObj.css({
-                            "margin-top" : marginV,
-                            "margin-left" : marginH
-                        });
-                    }
-
-                }, 50);
+                                mainDivObj.css({
+                                    "margin-top" : marginV,
+                                    "margin-left" : marginH
+                                });
+                            }
+                        }, 50);
             }
         };
 
         NxVPL.init();
         NxVPL.alignElements();
     }
+}
+
+function vplp_utils_move(inArray, inFrom, inTo) {
+    
+    if (inTo === inFrom) {
+        return;
+    }
+
+    var target = inArray[inFrom];
+    var increment = inTo < inFrom ? -1 : 1;
+
+    for (var k = inFrom; k != inTo; k += increment) {
+        inArray[k] = inArray[k + increment];
+    }
+    inArray[inTo] = target;
 }
